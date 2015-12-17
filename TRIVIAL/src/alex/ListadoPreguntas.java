3d967934e090a657cf7e9053a9c7ca93b2b1f4a3
Trosydman <carlosmesquita94@outlook.es>
2015-12-17 19:27:09 +0100

    He puesto mis preguntas y he dado algo de formato.

--- TRIVIAL/src/alex/ListadoPreguntas.java
+++ TRIVIAL/src/alex/ListadoPreguntas.java
@@ -36,6 +36,31 @@ public class ListadoPreguntas {
 			String opcAProgALE5=" Crear un método con el mismo nombre pero diferentes argumentos";
 			String opcBProgALE5=" Cambiarle el nombre dejándolo con la misma funcionalidad";
 			String opcCProgALE5=" Añadirle funcionalidades a un método";
+			
+		String pregProgCAR1="¿A qué hace referencia el comando 'syso'?";
+			String opcAProgCAR1="System.out.println();";
+			String opcBProgCAR1="System.out.println('');";
+			String opcCProgCAR1="System.out.println()";
+
+		String pregProgCAR2="Hemos aprendido que existen arrays unidimensionales y bidimensionales, pero...¿se pueden hacer arrays con más dimensiones?";
+			String opcAProgCAR2="Sí, se pueden hacer de infinitas dimensiones.";
+			String opcBProgCAR2="No, sólo se pueden hacer de las 2 formas mencionadas.";
+			String opcCProgCAR2="No, el límite está en 6 dimensiones.";
+
+		String pregProgCAR3="if(!isMenorDeEdad && calcetines.getColor().equals(Color.WHITE) && ropa.isFea?true:false)";
+			String opcAProgCAR3="cliente.darFajoBilletes()?System.out.println('No lo había reconocido Cristiano, pase y disfrute.');:System.out.println('NO ENTRAS');";
+			String opcBProgCAR3="System.out.println('NO ENTRAS');";
+			String opcCProgCAR3="System.out.println('Puedes entrar.');";
+
+		String pregProgCAR4="¿Qué es un String?";
+			String opcAProgCAR4="Un objeto, aunque le puedas pasar directamente una cadena";
+			String opcBProgCAR4="Un tipo de variable, ya que se le puede pasar directamente una cadena";
+			String opcCProgCAR4="Un atributo";
+
+		String pregProgCAR5="El Javadoc se genera automáticamente al correr el programa en Eclipse.";
+			String opcAProgCAR5="Falso, se crea de forma independiente.";
+			String opcBProgCAR5="verdadero.";
+			String opcCProgCAR5="Si se instala la extensión previamente sí, ya que no viene por defecto.";
 		
 		
 			
@@ -73,7 +98,30 @@ public class ListadoPreguntas {
 			String opcBBBDDALE5=" Visualiza los empleados que han realizado un número de pedidos igual a 10";
 			String opcCBBDDALE5=" Visualiza todos los empleados y el número de pedidos que han gestionado, incluyendo el pedido igual a 10";
 			
-			
+		String pregBBDDCAR1="¿Qué trimestre de BBDD viene con curvas?";
+			String opcABBDDCAR1="Todos vienen con curvas y vaselina gratis.";
+			String opcBBBDDCAR1="El 2º";
+			String opcCBBDDCAR1="El 1º";
+
+		String pregBBDDCAR2="¿Para que sirve la función SOUNDEX?";
+			String opcABBDDCAR2="Devuelve el valor fonético del texto.";
+			String opcBBBDDCAR2="Devuelve la posición del texto.";
+			String opcCBBDDCAR2="Devuelve el ex-valor del texto";
+
+		String pregBBDDCAR3="¿Cómo se consigue el produccto cartesiano de 2 tablas?";
+			String opcABBDDCAR3="FROM TABLA1, TABLA2";
+			String opcBBBDDCAR3="FROM TABLA1 JOIN TABLA2 USING(ID)";
+			String opcCBBDDCAR3="FROM TABLA1 RIGHT JOIN TABLA2";
+
+		String pregBBDDCAR4="¿Qué base de datos es una de las más liosas y con más tablas con que hemos trabajado?";
+			String opcABBDDCAR4="NORTHWIND";
+			String opcBBBDDCAR4="BUSES";
+			String opcCBBDDCAR4="SYSTEM";
+
+		String pregBBDDCAR5="¿Se puede escribir en minúscula las consultas de BBDD?";
+			String opcABBDDCAR5="Sí";
+			String opcBBBDDCAR5="No";
+			String opcCBBDDCAR5="Ninguna de las anteriores";
 		
 			
 			
@@ -83,7 +131,7 @@ public class ListadoPreguntas {
 			
 		//PREGUNTAS DE HTML	
 			
-			String pregHTMLALE1=" ¿Cuál es la forma correcta de insertar un comentario? ";
+		String pregHTMLALE1=" ¿Cuál es la forma correcta de insertar un comentario? ";
 			String opcAHTMLALE1=" <!--...--> ";
 			String opcBHTMLALE1=" <¡--...--!>";
 			String opcCHTMLALE1=" <comment>";
@@ -109,7 +157,30 @@ public class ListadoPreguntas {
 			String opcBHTMLALE5=" Incluirlos en un fichero externo vinculado a ese fichero HTML";
 			String opcCHTMLALE5=" Incluirlos en la sección cabecera ('head') para agruparlos en unmismo sitio en la misma página";
 			
-			
+		String pregHTMLCAR1="Sigue la frase: Header arriba, el contenido en medio y...";
+			String opcAHTMLCAR1="... footer abajo.";
+			String opcBHTMLCAR1="... no me maqueta la página.";
+			String opcCHTMLCAR1="... Rafa me pierdo.";
+
+		String pregHTMLCAR2="Comando para imprimir espacios en blanco en una página web.";
+			String opcAHTMLCAR2="&nbsp;";
+			String opcBHTMLCAR2="' '";
+			String opcCHTMLCAR2="white-space";
+
+		String pregHTMLCAR3="¿Qué etiquetas(entre otras), utilizamos en el head para que nos permita introducir carácteres con tilde y especiales?";
+			String opcAHTMLCAR3="<meta charset='UTF-8'>";
+			String opcBHTMLCAR3="<meta charset='UTF'>";
+			String opcCHTMLCAR3="<data charconfig='UTF8'>";
+
+		String pregHTMLCAR4="¿Qué comando de css sustituye a la etiqueta pre?";
+			String opcAHTMLCAR4="white-space: pre;";
+			String opcBHTMLCAR4="white-space: pro;";
+			String opcCHTMLCAR4="white space: pre;";
+
+		String pregHTMLCAR5="¿Qué le ocurre a Rafa si hay lenguaje a última hora?";
+			String opcAHTMLCAR5="Le entra el sueño.";
+			String opcBHTMLCAR5="Se enfada.";
+			String opcCHTMLCAR5="Le entre risa tonta.";
 			
 			
 			
@@ -145,7 +216,30 @@ public class ListadoPreguntas {
 				String opcBSISALE5=" Cambiarle el nombre dejándolo con la misma funcionalidad";
 				String opcCSISALE5=" Añadirle funcionalidades a un método";
 			
-			
+			String pregSISCAR1="Hemos trabajado con";
+				String opcASISCAR1="Arduino UNO";
+				String opcBSISCAR1="Arduino UNO!";
+				String opcCSISCAR1="Arduino UNO1";
+
+			String pregSISCAR2="Los RAIDs principales dados en clase son: RAID0, RAID1 y...";
+				String opcASISCAR2="RAID5";
+				String opcBSISCAR2="RAIDPRO";
+				String opcCSISCAR2="RAID2";
+
+			String pregSISCAR3="¿A qué 'muerte' se asemeja la muerte de Alan Turing?";
+				String opcASISCAR3="A la de Blancanieves.";
+				String opcBSISCAR3="A la de Bambi.";
+				String opcCSISCAR3="Ninguna de las otras.";
+
+			String pregSISCAR4="¿De qué año es el Windows Server que utilizamos en sistemas?";
+				String opcASISCAR4="2003";
+				String opcBSISCAR4="2000";
+				String opcCSISCAR4="2001";
+
+			String pregSISCAR5="Led IR es lo mismo que decir...";
+				String opcASISCAR5="Led de infra rojos";
+				String opcBSISCAR5="Led transparente";
+				String opcCSISCAR5="Led irrespetuoso";
 				
 				
 				
@@ -181,7 +275,30 @@ public class ListadoPreguntas {
 				String opcBCOLALE5=" Visualiza los empleados que han realizado un número de pedidos igual a 10";
 				String opcCCOLALE5=" Visualiza todos los empleados y el número de pedidos que han gestionado, incluyendo el pedido igual a 10";
 				
-			
+			String pregCOLCAR1="¿Qué cantidad media de mensajes de WhatsApp entran el día anterior de entregar un boletín de BBDD?";
+				String opcACOLCAR1="100-200";
+				String opcBCOLCAR1="0, nadie sabe hacerlo.";
+				String opcCCOLCAR1="5-10";
+
+			String pregCOLCAR2="¿Cuál fue la primera dinámica de grupo qué hicimos con Miguel?";
+				String opcACOLCAR2="Ambas cosas";
+				String opcBCOLCAR2="Comida de convivencia";
+				String opcCCOLCAR2="Preguntas personales a los compañeros y aviones de papel";
+
+			String pregCOLCAR3="¿Quienes reventaron un led RGB en sistemas?";
+				String opcACOLCAR3="Felipe y Rafa";
+				String opcBCOLCAR3="Fernando Carmona y Galiani";
+				String opcCCOLCAR3="Miguel Campos";
+
+			String pregCOLCAR4="¿Cómo se llama el portero?";
+				String opcACOLCAR4="Manolo";
+				String opcBCOLCAR4="Rafa";
+				String opcCCOLCAR4="Pedro";
+
+			String pregCOLCAR5="¿Qué foto se puso inicialmente en el grupo de WhatsApp de clase?";
+				String opcACOLCAR5="Alan Turing";
+				String opcBCOLCAR5="Don Bosco a lo Kanye West";
+				String opcCCOLCAR5="'Todos' en el Dominos";
 	
 	
 	
