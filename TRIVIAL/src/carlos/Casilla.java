f9673b0af26baa9b641e845eb49a7aa027db77e1
Trosydman <carlosmesquita94@outlook.es>
2015-12-19 18:59:46 +0100

    Modificación de atributos y constructores en Casilla He añadido unos atributos que son constantes que creo que facilitarán el reconocimientos del tipos de las casillas. Además he añadido unos  atributos que son las posiciones de los 4 diferentes Equipos en la  casilla, y he modificado el constructor.

--- TRIVIAL/src/carlos/Casilla.java
+++ TRIVIAL/src/carlos/Casilla.java
@@ -1,21 +1,33 @@
 package carlos;
 
 public class Casilla {
-	int posXEq1;
-	int posYEq1;
-	int posXEq2;
-	int posYEq2;
-	int posXEq3;
-	int posYEq3;
-	int posXEq4;
-	int posYEq4;
+	static final int PROG = 0;
+	static final int BBDD = 1;
+	static final int HTML = 2;
+	static final int SIS = 3;
+	static final int COL = 4;
 	
-	int tipo;
-	boolean quesito;
+	private int[] posEq1 = new int[2];
+	private int[] posEq2 = new int[2];
+	private int[] posEq3 = new int[2];
+	private int[] posEq4 = new int[2];
+	
+	private int tipo;
+	private boolean quesito;
 	
 	public Casilla(int posX, int posY, int tipo) {
-		this.posYEq1 = posX;
-		this.posXEq1 = posY;
+		this.posEq1[1] = posX;
+		this.posEq1[0] = posY;
+		
+		this.posEq2[1] = posX+4;
+		this.posEq2[0] = posY;
+		
+		this.posEq3[1] = posX;
+		this.posEq3[0] = posY+1;
+		
+		this.posEq4[1] = posX+4;
+		this.posEq4[0] = posY+1;
+		
 		this.tipo = tipo;
 	}
 
@@ -29,7 +41,11 @@ public class Casilla {
 
 	@Override
 	public String toString() {
-		return "Casilla [posX=" + posXEq1 + ", posY=" + posYEq1 + ", tipo=" + tipo + ", quesito=" + quesito + "]";
+		return "Casilla [\n\tposXEq1=" + posEq1[1] + ", posYEq1=" + posEq1[0] + "\n\tposXEq2=" + posEq2[1] + ", posYEq2=" + posEq2[0]
+				+ "\n\tposXEq3=" + posEq3[1] + ", posYEq3=" + posEq3[0] + "\n\tposXEq4=" + posEq4[1] + ", posYEq4=" + posEq4[0]
+				+ "\ntipo=" + tipo + ", quesito=" + quesito + "]\n";
 	}
+
+	
 	
 }
