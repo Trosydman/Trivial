aa5b2fd4ac895ee0619e5e5f3d92dbc67d14271e
rapindirve <rapindrive@hotmail.com>
2016-01-02 22:45:45 +0100

    Añadido de diseños títulos
    Voy añadiendo diseños a los títulos del menú

--- TRIVIAL/src/german/Disenio.java
+++ TRIVIAL/src/german/Disenio.java
@@ -1,20 +1,48 @@
 package german;
 
 public class Disenio {
-	// ATRIBUTOS
+	
+	
+	
 
+	public static void generarBienvenida(){
+		
+		System.out.println(" Bienvenidos a Trivial\n");
 	
+		System.out.println("   ____    _             _                   _   \n" +
+ | __ )  (_)   __ _    | |_    ___  __  __ | |_ 
+ |  _ \  | |  / _` |   | __|  / _ \ \ \/ / | __|
+ | |_) | | | | (_| |   | |_  |  __/  >  <  | |_ 
+ |____/  |_|  \__, |    \__|  \___| /_/\_\  \__|
+              |___/                             ");
+		
+	}
+		
+	public static void generarMenu (){
+		
+		System.out.println(" 1) Empezar a jugar.");
+		System.out.println(" 2) Reglas del juego.");
+		System.out.println(" 3) ¿Quienes somos?");
+		System.out.println(" 0) o 4) Salir del menú.;");
+		System.out.println("Elija una opción (0-3) o (1-4)");
+				
+	}
+		
 	
-	// CONSTRUCTORES
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
+		
+	}
 	
 	
-	// M�TODOS
 
-	
-	
-	// GETTERS, SETTERS Y TOSTRINGS
-
-	
-	
-}
