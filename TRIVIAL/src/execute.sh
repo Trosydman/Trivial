be5bedeb86ac4983a87f6939461efc6b5c5df0fe
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2023-10-19 12:53:30 +0200

    Create execute.sh to compile and run Trivial Salesianos game

---
+++ TRIVIAL/src/execute.sh
@@ -0,0 +1,21 @@
+rm leer_por_teclado/Leer.class
+rm alex/ListadoPreguntas.class
+rm german/Disenio.class
+rm norberto/Movimiento.class
+rm felipe/Trivial.class
+rm carlos/Equipo.class
+rm carlos/Casilla.class
+rm carlos/Tablero.class
+rm carlos/Principal.class
+
+javac leer_por_teclado/Leer.java
+javac alex/ListadoPreguntas.java
+javac german/Disenio.java
+javac norberto/Movimiento.java
+javac felipe/Trivial.java
+javac carlos/Equipo.java
+javac carlos/Casilla.java
+javac carlos/Tablero.java
+javac carlos/Principal.java
+
+java -cp . carlos/Principal
